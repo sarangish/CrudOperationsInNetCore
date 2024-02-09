331731444bab2094737442ebdb8a3b91bb2ff483
Admin <Admin@DESKTOP-VUF59TH>
2024-02-09 17:19:01 +0530

    Add project files.

---
+++ Models/BrandContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperationsInNetCore.Models
+{
+    public class BrandContext : DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> contextOptions) : base(contextOptions)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
